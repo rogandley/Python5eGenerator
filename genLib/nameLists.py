c72b592a5ac635aa535b4be76697d8022f7f5aa9
Robert Gandley <reg2763@cs.rit.edu>
2018-10-19 20:23:10 -0400

    Starting values for random name generator

---
+++ genLib/nameLists.py
@@ -0,0 +1,16 @@
+"""
+Name resources for generator
+"""
+
+
+firstOfFirst = ["Pa", "Dena", "Apa", "Tora", "Fara", "Alla", "Tana", "Rea", "Sari", "Corri", "Caro", "Vale", "Ro",    \
+                "Ju", "Curi", "Dani", "Ta", "Ave", "Sco", "Vena", "Eli", "Sacha", "Bai", "Lo", "Car", "Jinha", "Texi" \
+                "Ari", "Leva"]
+
+lastOfFirst = ["no", "ro", "ham", "ris", "ras", "lous", "n", "ley", "rine", "na", "ry", "ken", "men", "ntino", "el"]
+
+firstOfLast = ["Torch", "Whitten", "Long", "Da", "Torre", "Wea", "Fi", "Fire", "Gordon", "Dumble", "Free", "Dread"    \
+               "Fly", "Half", "Short", "Kell"]
+
+lastOfLast = ["wood", "vine", "birch", "spell", "spirit", "en", "dore", "'ar", "branch", "bringer", "slinger",       \
+               "oak", "brow", "belt", "delver", "heart", "chest", "tank", "arm", "hand", "foot", "fall"]
